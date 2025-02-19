ad3178832d9f7bcdab12587b37c63eca59b09fcb
psvoboda1987 <psvoboda1987@gmail.com>
2025-02-19 14:31:15 +0100

    Add files via upload
    separate index and post detail JS

--- post.js
+++ post.js
@@ -4,9 +4,14 @@ window.onload = async () => {
 
 async function renderPost() {
     const converter = new showdown.Converter();
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(location.search);
     const title = urlParams.get('article');
     await fetch(`pages/${title}/default.md`)
         .then(data => data.text())
         .then(data => document.getElementById('content').innerHTML = converter.makeHtml(data));
+
+    // fix img src
+    Array.from(document.images).forEach(image => {
+        image.src = image.src.replace(location.pathname.split('/')[1], `${location.pathname.split('/')[1]}/pages/${title}`);
+    })
 }

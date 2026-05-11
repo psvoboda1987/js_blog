d3496d6e86b78b3f625e22a8391ca177acb2a289
Petr <psvoboda1987@gmail.com>
2026-05-11 21:25:20 +0200

    fix light mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,40 +11,49 @@ setFontSize(getFontSize());
 function activateColorModeSwitch() {
     getElementById('colorMode').addEventListener('click', (e) => {
         setDocumentColorMode(e.target.dataset.mode);
+        saveDocumentColorMode(e.target.dataset.mode);
         changeColorModeSwitchElement(e.target.dataset.mode);
     });
 }
 
 function setColorModeFromStorage() {
-    let mode = localStorage.getItem('colorMode') || 'light';
-    changeColorModeSwitchElement(mode);
-    setDocumentColorMode(mode);
+    let mode = localStorage.getItem('colorMode');
+    if (mode !== null) {
+        setDocumentColorMode(mode);
+        saveDocumentColorMode(mode);
+        changeColorModeSwitchElement(mode);
+    }
 }
 
 function changeColorModeSwitchElement(mode) {
     let switchElement = getElementById('colorMode');
     if (mode === 'dark') {
-        switchElement.dataset.mode = 'light'; // activate light mode
-        switchElement.innerText = mode;
+        switchElement.dataset.mode = 'light';
+        switchElement.innerText = 'light';
     } else {
         switchElement.dataset.mode = 'dark';
-        switchElement.innerText = mode;
+        switchElement.innerText = 'dark';
     }
 }
 
 function setDocumentColorMode(mode) {
-    localStorage.setItem('colorMode', mode);
     document.body.className = mode;
+}
+
+function saveDocumentColorMode(mode) {
+    localStorage.setItem('colorMode', mode);
 }
 
 function activateFontSizeIncreaseButton() {
     getElementById('fontSizeIncrease').addEventListener('click', (e) => {
+        saveFontSize(getFontSize() + 1);
         setFontSize(getFontSize() + 1);
     });
 }
 
 function activateFontSizeDecreaseButton() {
     getElementById('fontSizeDecrease').addEventListener('click', (e) => {
+        saveFontSize(getFontSize() - 1);
         setFontSize(getFontSize() - 1);
     });
 }
@@ -60,8 +69,11 @@ function getFontSize() {
 }
 
 function setFontSize(fontSize) {
-    localStorage.setItem('fontSize', fontSize);
     document.body.style.fontSize = `${fontSize}px`;
+}
+
+function saveFontSize(fontSize) {
+    localStorage.setItem('fontSize', fontSize);
 }
 
 function getElementById(id) {
